3bd49a6866417eeb13e6035c242cc3a870ff103e
sanna-bjorn <92040325+sanna-bjorn@users.noreply.github.com>
2022-11-02 12:55:26 +0200

    styling and function to operate the calculator

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 // add functions for add, subtract, multiply, divide
 
-const add = function (a, b) {
-  result = a + b;
-  return result;
-};
+function add(a, b) {
+  return a + b;
+}
+console.log(add);
 
-const subtract = function (a, b) {
-  result = a - b;
-  return result;
-};
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract);
 
-const multiply = function (a, b) {
-  result = a * b;
-  return result;
-};
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply);
 
-const divide = function (a, b) {
-  result = a % b;
-  return result;
-};
+function divide(a, b) {
+  return a % b;
+}
+console.log(divide);
+
+// create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+// So when 1 is clicked event listener catches this and displays 1 and stores it as the first number. Could be a loop that breaks when = is pressed?
+
+function addButtonListener() {
+  document.querySelector("#one").onclick = 1;
+  // document.querySelector("#two").onclick = playerChosePaper;
+  // document.querySelector("#three").onclick = playerChoseScissors;
+}
+addButtonListener();
+console.log(addButtonListener);
