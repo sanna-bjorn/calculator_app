0d0320473396a46a20d42b69ec34336b64c381fc
sanna-bjorn <92040325+sanna-bjorn@users.noreply.github.com>
2022-11-02 09:46:27 +0200

    setting up functions for calculations

---
+++ main.js
@@ -0,0 +1,21 @@
+// add functions for add, subtract, multiply, divide
+
+const add = function (a, b) {
+  result = a + b;
+  return result;
+};
+
+const subtract = function (a, b) {
+  result = a - b;
+  return result;
+};
+
+const multiply = function (a, b) {
+  result = a * b;
+  return result;
+};
+
+const divide = function (a, b) {
+  result = a % b;
+  return result;
+};
